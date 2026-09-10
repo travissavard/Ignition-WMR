22e98eb52c2f3f05eb55c2140200b7f3c8063e6b
Travis Savard <travissavard@users.noreply.github.com>
2026-09-09 17:41:53 -0700

    Make PSVR2 registry setup conditional

--- support/launch_serverhelper.sh
+++ support/launch_serverhelper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -e
+
 unset SteamGameId
 
-# Add registry file for Ignition to ensure PSVR2 Sense controllers work
-./proton run reg import ./wine_psvr2_hidraw.reg
+# PSVR2 uses this registry tweak for Sense controllers. WMR/Oasis installs do
+# not copy the file, so their WMR-specific Wine/Proton runtime is left alone.
+if [ -f ./wine_psvr2_hidraw.reg ]; then
+    ./proton run reg import ./wine_psvr2_hidraw.reg
+fi
 
 ./proton run "$@"
